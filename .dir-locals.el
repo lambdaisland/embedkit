7dee3459b24ee54195c56c8df89e1ebc0ca51e08
Arne Brasseur <arne@arnebrasseur.net>
2021-02-12 09:23:44 +0100

    Boilerplate

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((nil . ((cider-clojure-cli-global-options     . "-A:dev:test")
+         (cider-preferred-build-tool           . clojure-cli)
+         (cider-redirect-server-output-to-repl . t)
+         (cider-repl-display-help-banner       . nil)
+         (clojure-toplevel-inside-comment-form . t))))
