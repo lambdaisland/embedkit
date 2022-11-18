88fa52a9e7243616a7a3638ed4d8bc2035d6e2ab
Laurence Chen <laurence@replware.dev>
2022-11-18 14:46:26 +0800

    move the development id/pw/embedded secret key to config.edn

--- repl_sessions/find_db.clj
+++ repl_sessions/find_db.clj
@@ -1,9 +1,8 @@
 (ns find-db
   (:require [lambdaisland.embedkit :as e]))
 
-(def conn (e/connect {:user "admin@example.com"
-                      :password "aqd4ijj4"
-                      :secret-key "6fa6b6600d27ff276d3d0e961b661fb3b082f8b60781e07d11b8325a6e1025c5"}))
+(def conn
+  (e/connect (read-string (slurp "dev/config.edn"))))
 
 (e/find-database conn "example_tenant")
 
