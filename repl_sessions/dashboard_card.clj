f97e2256dc485064223de2c4628edc6c0b301e81
Laurence Chen <laurence@replware.dev>
2023-07-30 00:06:11 +0800

    Improve example - repl_sessions/dashboard_card.clj
    use Metabase sample database as example

--- repl_sessions/dashboard_card.clj
+++ repl_sessions/dashboard_card.clj
@@ -12,27 +12,27 @@
 conn
 
 ;; find database by name
-(def db (e/find-database conn "example_tenant"))
+(def db (e/find-database conn "Sample Database"))
 (:id db)
 
 ;; populate cache
 (e/populate-cache conn)
 
 ;; create card
-(def card (e/native-card {:name "my new card 1"
+(def card (e/native-card {:name "order card"
                           :database (:id db)
-                          :sql "SELECT * FROM enzo.account"}))
+                          :sql "SELECT * FROM orders"}))
 card
 (r/browse! (e/find-or-create! conn card))
 
 ;; create dashboard with dashboard-card
-(let [card1 (e/native-card {:name "my new card 1"
+(let [card1 (e/native-card {:name "order card"
                             :database (:id db)
-                            :sql "SELECT * FROM enzo.account"})
-      card2 (e/native-card {:name "my new card 2"
+                            :sql "SELECT * FROM orders"})
+      card2 (e/native-card {:name "invoice card"
                             :database (:id db)
-                            :sql "SELECT * FROM enzo.currency"})
-      dash (e/find-or-create! conn (e/dashboard {:name "Bar"
+                            :sql "SELECT * FROM invoices"})
+      dash (e/find-or-create! conn (e/dashboard {:name "Sample DB dashboard"
                                                  :cards [{:card card1
                                                           :x 0 :y 0
                                                           :width 5 :height 5}
